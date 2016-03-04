9ce1ff16f13c4e496597ac704258aaba12eb60a3
christian otkjær <christian.otkjaer@gmail.com>
2016-03-04 11:33:30 +0100

    Initial Commit

---
+++ Picker/Picker.h
@@ -0,0 +1,19 @@
+//
+//  Picker.h
+//  Picker
+//
+//  Created by Christian Otkjær on 04/03/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Picker.
+FOUNDATION_EXPORT double PickerVersionNumber;
+
+//! Project version string for Picker.
+FOUNDATION_EXPORT const unsigned char PickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Picker/PublicHeader.h>
+
+
